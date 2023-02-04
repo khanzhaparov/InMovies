a1c711970151ff55e57fc483ca9b6cc08ccbceaf
khanzhaparov <khanzhaparov@gmail.com>
2023-02-04 17:59:07 +0600

    commits

---
+++ src/components/styles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@mui/styles';
+
+export default makeStyles(() => ({
+  root: {
+    display: 'flex',
+    height: '100%',
+  },
+  toolbar: {
+    height: '70px',
+  },
+}));
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/App';
 
-createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
